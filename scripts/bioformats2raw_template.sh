861ee6ca617ac52d8ba436d805ae414620759ffe
Jason Weirather <jason-weirather@users.noreply.github.com>
2023-04-11 12:45:59 -0400

    clean up unecessary lines

--- scripts/bioformats2raw_template.sh
+++ scripts/bioformats2raw_template.sh
@@ -3,8 +3,3 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 JAR_DIR="$(dirname "$SCRIPT_DIR")"
 JAR_FILE=$(find "$JAR_DIR/libs" -iname 'bioformats2raw*.jar' | head -n 1)
 java -jar "$JAR_FILE" "$@"
-
-
-#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-#JAR_DIR="$(dirname "$SCRIPT_DIR")"
-#java -jar "$JAR_DIR/libs/bioformats2raw-unspecified.jar" "$@"
