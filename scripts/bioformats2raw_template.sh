af2ac6127bb5af45e703a02236053c8ce92bba87
Jason Weirather <jason-weirather@users.noreply.github.com>
2023-04-11 12:27:45 -0400

    update to work on mac

--- scripts/bioformats2raw_template.sh
+++ scripts/bioformats2raw_template.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 JAR_DIR="$(dirname "$SCRIPT_DIR")"
-java -jar "$JAR_DIR/libs/bioformats2raw-0.6.1.jar" "$@"
+JAR_FILE=$(find "$JAR_DIR/libs" -iname 'bioformats2raw*.jar' | head -n 1)
+java -jar "$JAR_FILE" "$@"
+
+
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#JAR_DIR="$(dirname "$SCRIPT_DIR")"
+#java -jar "$JAR_DIR/libs/bioformats2raw-unspecified.jar" "$@"
